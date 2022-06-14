ac066bb81a80c6b658fac5b3a4d08983dbb54d68
tarun nahak <tarunnahak25@gmail.com>
2022-06-14 13:44:28 +0530

    added navbar component

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,22 @@
+/* eslint-disable quotes */
 import "../styles/globals.css";
+import Script from "next/script";
+import { ThemeProvider } from "next-themes";
 
-const MyApp = ({ Component, pageProps }) => <Component {...pageProps} />;
+import { Navbar, Footer } from "../components";
+
+const MyApp = ({ Component, pageProps }) => (
+  <ThemeProvider attribute="class">
+    <div className="dark:bg-nft-dark bg-white min-h-min">
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+    <Script
+      src="https://kit.fontawesome.com/1d292e90a6.js"
+      crossOrigin="anonymous"
+    />
+  </ThemeProvider>
+);
 
 export default MyApp;
