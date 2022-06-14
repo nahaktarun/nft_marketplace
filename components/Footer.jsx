7ee43e077b74b7cf7f8fa331bed8c2be8d1ba86a
tarun nahak <tarunnahak25@gmail.com>
2022-06-14 15:04:02 +0530

    added the footer component

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,5 +1,100 @@
-import React from "react";
+/* eslint-disable quotes */
+import Image from "next/image";
+import { useTheme } from "next-themes";
 
-const Footer = () => <div>Footer</div>;
+import images from "../assets";
+import { Button } from ".";
+
+const FooterLinks = ({ heading, items }) => (
+  <div className="flex-1 justify-start items-start mt-2">
+    <h3 className="font-popping dark:text-white text-nft-black-1 font-semibold text-xl mb-8">
+      {heading}
+    </h3>
+    {items.map((item, index) => (
+      <p className="font-poppins dark:text-white text-nft-black-1 font-normal text-base cursor-pointer dark:hover:text-nft-gray-1 hover:text-nft-black-1 my-3">
+        {item}
+      </p>
+    ))}
+  </div>
+);
+const Footer = () => {
+  const { theme } = useTheme();
+  return (
+    <footer className="flexCenter flex-col border-t dark:border-nft-black-1 boder-nft-gray-1 sm:py-8 py-16">
+      <div className="w-full minmd:w-4/5 flex flex-row sm:px-4 px-16">
+        <div className="flexstart flex-1 flex-col">
+          <div className="flexcenter cursor-pointer">
+            <Image
+              src={images.logo02}
+              objectFit="contain"
+              width={32}
+              height={32}
+              alt="logo"
+            />
+            <p className="dark:text-white text-nft-black-1 font-semibold text-lg ml-1">
+              CryptoCart
+            </p>
+          </div>
+          <div>
+            <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-base mt-6 ">
+              Get the latest update
+            </p>
+            <div className="flexBetween md:w-full minlg:w-557 w-357 mt-6 dark:bg-nft-black-2 bg-white border dark:border-nft-black-2 border-nft-gray-2 rounded-md">
+              <input
+                type="email"
+                placeholder="Your Email"
+                className="h-full flex-1 w-full dark:bg-nft-black-2 bg-white px-4 rounded-md dark:text-white text-nft-black-1 font-normal text-xs minlg:text-lg outline-none"
+              />
+              <div className="flex-initial">
+                <Button btnName="Email me" classStyles="rounded-md" />
+              </div>
+            </div>
+          </div>
+          <div className="flex-1 flexBetweenStart flex-wrap ml-10 md:ml-0 md:mt-8">
+            <FooterLinks
+              heading="CryptoCart"
+              items={["Explore", "How it works?", "Contact Us"]}
+            />
+            <FooterLinks
+              heading="Support"
+              items={[
+                "Help Center",
+                "Terms of Service",
+                "Legal",
+                "privacy Policy",
+              ]}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="flexCenter w-full mt-5 border-t dark:border-nft-black-1 border-nft-gray-1 sm:px-4 px-16">
+        <div className="flexBetween flex-row w-full minmd:w-4/5 sm:flex-col mt-7">
+          <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-base">
+            Cryptocart, Inc. All right Reserved.
+          </p>
+          <div className="flex flex-row sm:mt-4">
+            {[
+              images.instagram,
+              images.twitter,
+              images.telegram,
+              images.discord,
+            ].map((image, i) => (
+              <div className="mx-2 cursor-pointer " key={i}>
+                <Image
+                  src={image}
+                  objectFit="contain"
+                  width={24}
+                  height={24}
+                  alt="social"
+                  className={theme === "light" && "filter invert"}
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
