2c6019c0e0aadfd01733339ce8a579e4c0aa0875
tarun nahak <tarunnahak25@gmail.com>
2022-06-22 00:57:00 +0530

    added the solidity configuration

---
+++ contracts/NFTMarketplace.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.15;
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+import "hardhat/console.sol";
+
+contract NFTMarketplace is ERC721URIStorage {
+
+    using Counters for Counters.Counter;
+
+    Counters.Counter  private _tokenIds;
+    Counters.Counter  private _itemsSold;
+    
+    // listing price
+    uint256 listingPrice = 0.025 ether;
+
+    address payable owner;
+
+    mapping(uint256 => MarketItem) private idToMarketItem;
+
+    struct MarketItem {
+        uint256 tokenId;
+        address payable seller;
+        address payable owner;
+        uint256 price;
+        bool sold;
+    }
+    event MarketItemCreated(
+        uint256  indexed tokenId,
+        address payable seller,
+        address payable owner,
+        uint256 price,
+        bool sold
+    );
+
+    constructor() {
+        owner = payable(msg.sender);
+    }
+}
