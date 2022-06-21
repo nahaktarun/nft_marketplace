04bf32df8261990af93b17f3f907dbdd05b159eb
tarun nahak <tarunnahak25@gmail.com>
2022-06-22 01:30:37 +0530

    added the resell ,createItem, createmarketsell functionality

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -39,4 +39,61 @@ contract NFTMarketplace is ERC721URIStorage {
     constructor() {
         owner = payable(msg.sender);
     }
+
+    function updateListingPrice(uint _listingPrice) public payable {
+        require(owner == msg.sender,"Only Marketplace Owner can update the listing price.");
+
+        listingPrice = _listingPrice;
+
+    }
+    function getListingPrice() public view  returns (uint256) {
+        return listingPrice;
+    }
+
+    function createToken(string memory tokenURI, uint256 price) public payable returns(uint){
+        _tokenIds.increment();
+
+        uint256 newTokenId = _tokenIds.current();
+
+        _mint(msg.sender,newTokenId);
+        _setTokenURI(newTokenId, tokenURI);
+
+        createMarketItem(newTokenId,price);
+
+        return newTokenId;
+    }
+
+    function createMarketItem(uint256 tokenId, uint256 price) private {
+
+        require(price>0,"Price Must be at least 1");
+        require(msg.value == listingPrice, "Price must be equal to listing price");
+
+        idToMarketItem[tokenId] = MarketItem(
+            tokenId,
+            payable(msg.sender),
+            payable(address(this)),
+            price,
+            false
+        );
+
+        _transfer(msg.sender, address(this), tokenId);
+        emit MarketItemCreated(tokenId, msg.sender, address(this),price,false);
+    }
+
+    function resellToken(uint256 tokenId, uint256 price) public payable{
+        require(idToMarketItem[tokenId].owner == msg.sender ,"Only item owner can perform this operation");
+
+        require(msg.value == listingPrice ,"Price must be equal to listing price");
+
+        idToMarketItem[tokenId].sold = false;
+        idToMarketItem[tokenId].price = price;
+        idToMarketItem[tokenId].seller = payable(msg.sender);
+        idToMarketItem[tokenId].owner = payable(address(this));
+
+        _itemsSold.decrement();
+
+        _transfer(msg.sender, address(this), tokenId);
+
+
+    }
 }
