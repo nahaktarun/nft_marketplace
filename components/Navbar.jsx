a6fb4cce9c6343c9f4cfd6977b0b5a464c1bc6b8
tarun nahak <tarunnahak25@gmail.com>
2022-06-28 16:12:54 +0530

    added the connection to wallet

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,6 +5,8 @@ import { useTheme } from "next-themes";
 import Image from "next/image";
 import Link from "next/link";
 
+import { NFTContext } from "../context/NFTContext";
+
 import images from "../assets";
 import { Button } from ".";
 
@@ -47,9 +49,9 @@ const MenuItems = ({ isMobile, active, setActive }) => {
   );
 };
 const ButtonGroup = ({ setActive, router }) => {
-  const hasConnected = true;
-
-  return hasConnected ? (
+  const { connectWallet, currentAccount } = useContext(NFTContext);
+  console.log({ currentAccount });
+  return currentAccount ? (
     <Button
       btnName="Create"
       classStyles="mx-2 rounded-xl"
@@ -62,7 +64,7 @@ const ButtonGroup = ({ setActive, router }) => {
     <Button
       btnName="Connect"
       classStyles="mx-2 rounded-xl"
-      handleClick={() => {}}
+      handleClick={connectWallet}
     />
   );
 };
